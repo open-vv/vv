0b7c9b1e1215634b02cbd38d4e4ba101d6111ba8
schaerer <schaerer>
2010-03-25 09:29:06 +0000

    added the new headers

--- vv/vvImageMapToWLColors.h
+++ vv/vvImageMapToWLColors.h
@@ -1,3 +1,20 @@
+/*=========================================================================
+  Program:   vv                     http://www.creatis.insa-lyon.fr/rio/vv
+
+  Authors belong to: 
+  - University of LYON              http://www.universite-lyon.fr/
+  - Léon Bérard cancer center       http://oncora1.lyon.fnclcc.fr
+  - CREATIS CNRS laboratory         http://www.creatis.insa-lyon.fr
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the copyright notices for more information.
+
+  It is distributed under dual licence
+
+  - BSD        See included LICENSE.txt file
+  - CeCILL-B   http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+======================================================================-====*/
 #ifndef vvImageMapToWLColors_h
 #define vvImageMapToWLColors_h
 #include <vtkImageMapToWindowLevelColors.h>
