421793325fd0ca0570ad3db66c8d216116945472
bharath <bharath>
2010-05-19 17:16:58 +0000

    Added functionalitites to Reset, save and view transformations etc..

--- vv/vvToolRigidReg.h
+++ vv/vvToolRigidReg.h
@@ -25,6 +25,7 @@
 #include "vvMainWindowBase.h"
 #include "vvMainWindow.h"
 #include "ui_vvToolRigidReg.h"
+#include "vtkMatrix4x4.h"
 
 //------------------------------------------------------------------------------
 class vvToolRigidReg:
@@ -52,20 +53,43 @@ public slots:
   void SetXvalue();
   void SetYvalue();
   void SetZvalue();
+  void SetOrderXtrans();
+  void SetOrderYtrans();
+  void SetOrderZtrans();
+  void SetOrderXrot();
+  void SetOrderYrot();
+  void SetOrderZrot();
   void UpdateXtranslider();
+  void UpdateXtransb();
   void UpdateYtranslider();
+  void UpdateYtransb();
   void UpdateZtranslider();
+  void UpdateZtransb();
   void UpdateXrotslider();
+  void UpdateXrotsb();
   void UpdateYrotslider();
+  void UpdateYrotsb(); 
   void UpdateZrotslider();
+  void UpdateZrotsb();  
   void SetOverlay();
-protected:
+  void SaveFile();
+  void ReadFile();
+  void ResetTransform();
+  void SetRotationCenter();
+  void SetSliderRanges();
+  void UpdateTextEditor(vtkMatrix4x4 *matrix,QString SetOrder);
+  void InitializeSliders();
+  
+  protected:
   Ui::vvToolRigidReg ui;
   vvSlicerManager * mInput1;
   vvSlicerManager * mInput2;
   vvMainWindow * mWindow;
+  QString mOrder;
   bool mTwoInputs;
+  std::vector<int> mImageSize;
   void SetTransform(double tX, double tY, double tZ, double aX, double aY, double aZ);
+  void SetTransform(vtkMatrix4x4 * matrix);
 }; // end class vvToolRigidReg
 //------------------------------------------------------------------------------
 
