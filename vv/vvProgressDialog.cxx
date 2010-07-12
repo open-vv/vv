ee318a6de9c515e567f56b84d3f3789ef0881810
dsarrut <dsarrut>
2010-07-12 06:58:18 +0000

    *** empty log message ***

---
+++ vv/vvProgressDialog.cxx
@@ -0,0 +1,60 @@
+/*=========================================================================
+  Program:   vv                     http://www.creatis.insa-lyon.fr/rio/vv
+
+  Authors belong to:
+  - University of LYON              http://www.universite-lyon.fr/
+  - Léon Bérard cancer center       http://oncora1.lyon.fnclcc.fr
+  - CREATIS CNRS laboratory         http://www.creatis.insa-lyon.fr
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the copyright notices for more information.
+
+  It is distributed under dual licence
+
+  - BSD        See included LICENSE.txt file
+  - CeCILL-B   http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+  ======================================================================-====*/
+
+// vv
+#include "vvProgressDialog.h"
+
+//------------------------------------------------------------------------------
+vvProgressDialog::vvProgressDialog(std::string message, bool show_progress) 
+{
+  setupUi(this);
+  connect(mCancelButton, SIGNAL(rejected()), this, SLOT(reject()));
+  textLabel->setText(message.c_str());
+  if (show_progress) 
+    progressBar->show();
+  else 
+    progressBar->hide();
+  this->show();
+}
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+void vvProgressDialog::SetText(std::string message)
+{
+  textLabel->setText(message.c_str());
+}
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+void vvProgressDialog::AddToText(std::string message)
+{
+  textLabel->setText(QString("%1\n%2").arg(textLabel->text()).arg(message.c_str()));
+}
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+void vvProgressDialog::SetProgress(unsigned int current,unsigned int max)
+{
+  progressBar->setMaximum(max);
+  progressBar->setValue(current);
+}
+//------------------------------------------------------------------------------
+
---
+++ vv/vvThreadedFilter.cxx
@@ -0,0 +1,105 @@
+/*=========================================================================
+  Program:   vv                     http://www.creatis.insa-lyon.fr/rio/vv
+
+  Authors belong to:
+  - University of LYON              http://www.universite-lyon.fr/
+  - Léon Bérard cancer center       http://oncora1.lyon.fnclcc.fr
+  - CREATIS CNRS laboratory         http://www.creatis.insa-lyon.fr
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the copyright notices for more information.
+
+  It is distributed under dual licence
+
+  - BSD        See included LICENSE.txt file
+  - CeCILL-B   http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+  ======================================================================-====*/
+
+// vv
+#include "vvThreadedFilter.h"
+#include "vvProgressDialog.h"
+
+// Qt
+#include <QMessageBox>
+
+//------------------------------------------------------------------------------
+vvThreadedFilter::vvThreadedFilter():
+  QThread()
+{
+  DD("vvThreadedFilter");
+  m_Filter = NULL;
+}
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+vvThreadedFilter::~vvThreadedFilter()
+{
+}
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+void vvThreadedFilter::SetFilter(clitk::ImageToImageGenericFilterBase * f)
+{
+  DD("SetFilter");
+  m_Filter = f;
+}
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+void vvThreadedFilter::Update()
+{
+  assert(m_Filter != NULL);
+
+  // Show a progress bar while computing
+  vvProgressDialog progress("Computing ...",100);
+  connect(&progress, SIGNAL(rejected()), this, SLOT(reject()));
+  this->start();
+  this->setTerminationEnabled(true);
+  std::string temp;
+  while (this->isRunning()) {
+    DD(this->isRunning());
+    m_FilterBase = m_Filter->GetFilterBase(); // get filterbase only after Update
+    if (m_FilterBase != NULL) {
+      progress.SetProgress(m_FilterBase->GetCurrentStepNumber(), 
+			   m_FilterBase->GetNumberOfSteps());
+      if (temp != m_FilterBase->GetCurrentStepName()) {
+	progress.AddToText(m_FilterBase->GetCurrentStepName());
+      }
+      temp = m_FilterBase->GetCurrentStepName();
+    }
+    this->wait(200); // in milisecond
+    qApp->processEvents();
+  }
+  DD("after loop");
+}
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+void vvThreadedFilter::run()
+{
+  assert(m_Filter != NULL);
+  m_Filter->Update();
+}
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+void vvThreadedFilter::reject()
+{
+  DD("vvThreadedFilter::reject");
+  this->quit();
+  // if (m_Filter != NULL) {
+//     m_Filter->MustStop();
+//   }
+  DD("after terminate");
+}
+//------------------------------------------------------------------------------
+
+
+
+
