16f9cff32207e43328678cdb17a0bccc3441b6cb
srit <srit>
2010-09-16 12:37:34 +0000

    Changed headers

--- itk/itkBSplineInterpolateImageFunctionWithLUT.h
+++ itk/itkBSplineInterpolateImageFunctionWithLUT.h
@@ -1,23 +1,22 @@
+/*=========================================================================
+  Program:   vv                     http://www.creatis.insa-lyon.fr/rio/vv
+
+  Authors belong to:
+  - University of LYON              http://www.universite-lyon.fr/
+  - Léon Bérard cancer center       http://oncora1.lyon.fnclcc.fr
+  - CREATIS CNRS laboratory         http://www.creatis.insa-lyon.fr
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the copyright notices for more information.
+
+  It is distributed under dual licence
+
+  - BSD        See included LICENSE.txt file
+  - CeCILL-B   http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+======================================================================-====*/
 #ifndef ITKBSPLINEINTERPOLATEIMAGEFUNCTIONWITHLUT_H
 #define ITKBSPLINEINTERPOLATEIMAGEFUNCTIONWITHLUT_H
-
-/* =========================================================================
-                                                                                
-  @file   itkBSplineInterpolateImageFunctionWithLUT.h
-  @author David Sarrut <david.sarrut@creatis.insa-lyon.fr>
-
-  Copyright (c) 
-  * CREATIS (Centre de Recherche et d'Applications en Traitement de l'Image). 
-    All rights reserved. See Doc/License.txt or
-    http://www.creatis.insa-lyon.fr/Public/Gdcm/License.html for details.
-  * Léon Bérard cancer center, 28 rue Laënnec, 69373 Lyon cedex 08, France
-  * http://www.creatis.insa-lyon.fr/rio
-                                                                                
-  This software is distributed WITHOUT ANY WARRANTY; without even the
-  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-  PURPOSE.  See the above copyright notices for more information.
-                                                                             
-========================================================================= */
 
 #include "itkBSplineWeightsCalculator.h"
 #include <itkBSplineInterpolateImageFunction.h>
