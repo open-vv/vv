9d10a018cb5d01de3f62b283eea1916c97d7fa44
dsarrut <dsarrut>
2010-10-15 06:15:21 +0000

    -add dependencies for all autotools

--- tools/clitkImageArithmGenericFilter.cxx
+++ tools/clitkImageArithmGenericFilter.cxx
@@ -17,16 +17,14 @@
 ======================================================================-====*/
 #ifndef CLITKIMAGEARITHMGENERICFILTER_CXX
 #define CLITKIMAGEARITHMGENERICFILTER_CXX
-/**
- -------------------------------------------------------------------
- * @file   clitkImageArithmGenericFilter.cxx
- * @author David Sarrut <David.Sarrut@creatis.insa-lyon.fr>
- * @date   23 Feb 2008
-
- * @brief
- -------------------------------------------------------------------*/
 
 #include "clitkImageArithmGenericFilter.h"
+#include "clitkImageArithm_ggo.h"
 
+namespace clitk {
+  // Specialisation
+  template<>
+  class ImageArithmGenericFilter<args_info_clitkImageArithm>;
+}
 
 #endif //define CLITKIMAGEARITHMGENERICFILTER_CXX
