cb5f6e9c4ca38eb5a304eb2b278ce049d2ca838b
dsarrut <dsarrut>
2011-04-07 13:49:35 +0000

    Romulo: - Including automated test procedures in clitk - Addition of test data   + build/tests is the default working directory set by CMake when running tests

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/romulo/creatis/clitk3/tests
+# Build directory: /home/romulo/creatis/clitk3/build/tests
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(clitkImageInfoTest "/home/romulo/creatis/clitk3/build/bin/clitkImageInfoTest")
+ADD_TEST(clitkWriteDicomSeriesTest "/home/romulo/creatis/clitk3/build/bin/clitkWriteDicomSeriesTest")
