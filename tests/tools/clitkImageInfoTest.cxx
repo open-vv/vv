49ae46d2b5aed3962b711b76b93ebb739baa1d30
maxime pech <maxime.pech@insa-lyon.fr>
2011-05-17 14:34:53 +0200

    tests with google tests coupled with cdash reporting

--- tests/tools/clitkImageInfoTest.cxx
+++ tests/tools/clitkImageInfoTest.cxx
@@ -15,57 +15,30 @@
   - BSD        See included LICENSE.txt file
   - CeCILL-B   http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
 ===========================================================================*/
-#include <cstdlib>
-#include <cstdio>
-
 #include <iostream>
 #include <sstream>
 #include <string>
-
+#include <fstream>
+#include "gtest/gtest.h"
+#include "stdio.h"
 #include <itksys/SystemTools.hxx>
-
-
-const size_t NUMTESTS=2;
-
-// test files
-const char mhd_files[NUMTESTS][128] = {
-  CLITK_DATA_PATH"/4d/mhd/00.mhd",
-  CLITK_DATA_PATH"/4d/mhd/bh.mhd"
-};
-
-// pre-written validation files. the idea
-// is that the output generated from the test
-// files match the verification files
-const char validation_files[NUMTESTS][128] = {
-  CLITK_DATA_PATH"/tools/clitkImageInfoTestValidate3D.out",
-  CLITK_DATA_PATH"/tools/clitkImageInfoTestValidate4D.out"
-};
-
-int main(int argc, char** argv)
-{
-  system("pwd");
+static inline void compare(std::string mhd, std::string ref){
+  std::ostringstream cmd_line;
+  ASSERT_TRUE(itksys::SystemTools::FileExists(mhd.c_str(), true));
+  cmd_line << TOOLS_PATH<<"clitkImageInfo " << mhd << " > clitkImageInfoTest.out";
+  std::cout << "Executing " << cmd_line.str() << std::endl;
+  system(cmd_line.str().c_str());
   
-  bool failed = false;
-  for (size_t i = 0; i < NUMTESTS; i++) {
-    std::ostringstream cmd_line;
-    cmd_line << "clitkImageInfo " << mhd_files[i] << " > clitkImageInfoTest.out";
-
-    std::cout << "Executing " << cmd_line.str() << std::endl;
-    system(cmd_line.str().c_str());
-    
-    // compare output with validation file
-    std::cout << "Validating output against " << validation_files[i] << std::endl;
-    bool differ = itksys::SystemTools::FilesDiffer("clitkImageInfoTest.out", validation_files[i]);
-    if (differ)
-    {
-      failed = true;
-      std::cout << "FAILED: Program output and reference do not match." << std::endl;
-    }
-    else
-    {
-      itksys::SystemTools::RemoveFile("clitkImageInfoTest.out");
-      std::cout << "PASSED" << std::endl;
-    }
-  }
-  return failed ? -1 : 0;
+  bool differ = itksys::SystemTools::FilesDiffer("clitkImageInfoTest.out", ref.c_str());
+  remove("clitkImageInfoTest.out");
+  EXPECT_FALSE(differ);
+}
+TEST(clitkImageInfoTest, main){
+  std::string mhd1=std::string(CLITK_DATA_PATH)+"4d/mhd/00.mhd";
+  std::string ref1=std::string(CLITK_DATA_PATH)+"tools/clitkImageInfoTestValidate3D.out";
+  compare(mhd1, ref1);
+  
+  std::string mhd2=std::string(CLITK_DATA_PATH)+"4d/mhd/bh.mhd";
+  std::string ref2=std::string(CLITK_DATA_PATH)+"tools/clitkImageInfoTestValidate4D.out";
+  compare(mhd2, ref2);
 }
---
+++ tests/vv/vvRegisterFormTest.cxx
@@ -0,0 +1,52 @@
+/*=========================================================================
+  Program:   vv                     http://www.creatis.insa-lyon.fr/rio/vv
+
+  Authors belong to:
+  - University of LYON              http://www.universite-lyon.fr/
+  - Léon Bérard cancer center       http://www.centreleonberard.fr
+  - CREATIS CNRS laboratory         http://www.creatis.insa-lyon.fr
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the copyright notices for more information.
+
+  It is distributed under dual licence
+
+  - BSD        See included LICENSE.txt file
+  - CeCILL-B   http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+===========================================================================*/
+#include "vvRegisterForm.h"
+#include <QUrl>
+#include <QSettings>
+#include "gtest/gtest.h"
+#include "vvCommon.h"
+#include <stdio.h>
+#include <fstream>
+const std::string data_path = "/home/mpech/workspace/vvSource/tests/data/";
+TEST(vvRegisterForm, canPush){
+  QUrl url;
+  //the file exists with some old lines
+  vvRegisterForm* v=new vvRegisterForm(url, QString::fromStdString(data_path+".settingsCanPush.txt"), QSettings::NativeFormat);
+  std::ifstream inFile((data_path+".settingsCanPush.txt").c_str(), std::ifstream::in);
+  ASSERT_FALSE(inFile.fail());
+  EXPECT_TRUE(v->canPush());
+  
+  //the file doesn't exist
+  vvRegisterForm* v2=new vvRegisterForm(url, QString::fromStdString(data_path+".settings_notFound.txt"), QSettings::NativeFormat);
+  EXPECT_TRUE(v2->canPush());
+  
+  //the current version is the same
+  QString strSettings=QString::fromStdString(data_path+".settings2.txt");
+  QSettings settings(strSettings, QSettings::NativeFormat);
+  settings.setValue("vvVersion", VV_VERSION);
+  vvRegisterForm* v3=new vvRegisterForm(url, strSettings, QSettings::NativeFormat);
+  EXPECT_FALSE(v3->canPush());
+}
+TEST(vvRegisterForm, acquitPushed){
+  QUrl url;
+  vvRegisterForm* v=new vvRegisterForm(url, QString::fromStdString(data_path+".settingsAcquit.txt"), QSettings::NativeFormat);
+  ASSERT_TRUE(v->canPush());
+  v->acquitPushed();
+  EXPECT_FALSE(v->canPush());
+  remove((data_path+".settingsAcquit.txt").c_str());
+}
