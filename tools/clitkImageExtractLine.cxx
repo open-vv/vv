bc61697818fa0f090c199bd169b286e46e2e0f26
mpech <maxime.pech@insa-lyon.fr>
2011-06-14 13:26:15 +0200

    windows compatibility for tools  (size_t vs uint...) and tests (.lib vs .a)

--- tools/clitkImageExtractLine.cxx
+++ tools/clitkImageExtractLine.cxx
@@ -137,7 +137,7 @@ int main(int argc, char * argv[])
       DD(direction);
       
       std::vector<double> val(dim[b]);
-	for (uint i=0; i<dim[b]; i++)
+	for (size_t i=0; i<dim[b]; i++)
 	  val[i]=0;
 	
       int k;
