12317efdc0a451f877e41a13c26d56931798af0b
mpech <maxime.pech@insa-lyon.fr>
2011-06-29 13:31:10 +0200

    Modification for nightly builds

--- tests/tools/clitkAffineTransformTest.cxx
+++ tests/tools/clitkAffineTransformTest.cxx
@@ -24,18 +24,34 @@
 #include <itksys/SystemTools.hxx>
 static inline void compare(std::string mhd, std::string refMhd, std::string refRaw){
   std::ostringstream cmd_line;
+  
+  //Ensure the description file exists
   ASSERT_TRUE(itksys::SystemTools::FileExists(mhd.c_str(), true));
+  
+  //Ensure the description reference file exists
   ASSERT_TRUE(itksys::SystemTools::FileExists(refMhd.c_str(), true));
+  
+  //Ensure our reference raw exists
   ASSERT_TRUE(itksys::SystemTools::FileExists(refRaw.c_str(), true));
+//todo : test this on windows  
+  //Generate the new mhd and the new raw
   cmd_line << TOOLS_PATH<<"clitkAffineTransform -i " << mhd << " -o "<<"out.mhd >/dev/null";
   system(cmd_line.str().c_str());
+  
+  //Check the new file has been created
   EXPECT_TRUE(itksys::SystemTools::FileExists("out.mhd", true));
+  
+  //Check the mhd and the produced mhd are still equal
   EXPECT_FALSE( itksys::SystemTools::FilesDiffer("out.mhd", refMhd.c_str())  );
-  EXPECT_FALSE( itksys::SystemTools::FilesDiffer("out.raw", refRaw.c_str())  );
+  
+  //Check the raw and the produced raw are different
+  EXPECT_TRUE( itksys::SystemTools::FilesDiffer("out.raw", refRaw.c_str())  );
+  
+  //clean the produced junk
   remove("out.mhd");
   remove("out.raw");
 }
-TEST(clitkAffineTransformTest, main){
+TEST(identity, clitkAffineTransformTest){
   std::string mhd1=std::string(CLITK_DATA_PATH)+"Deformation4D.mhd";
   std::string refMhd1=std::string(CLITK_DATA_PATH)+"Deformation4DRef.mhd";
   std::string refRaw1=std::string(CLITK_DATA_PATH)+"Deformation4DRef.raw";
