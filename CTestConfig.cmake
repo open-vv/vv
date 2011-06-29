9a6fbe1cfe80878083f816bfdb4560e7dfb62f79
mpech <maxime.pech@insa-lyon.fr>
2011-06-29 17:29:36 +0200

    publishing to vv's dashboard

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,6 +2,6 @@ set(CTEST_PROJECT_NAME "vv_itk4")
 set(CTEST_NIGHTLY_START_TIME "1:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www.cdash.org")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Insight")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=VV")
 set(CTEST_DROP_SITE_CDASH TRUE)
