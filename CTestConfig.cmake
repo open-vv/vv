7861c7dc29ff0f8cdfd7a929034eb63cc29d6703
mpech <maxime.pech@insa-lyon.fr>
2011-07-06 11:05:29 +0200

    Allow submit to other dashboard than vv's dashboard

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,18 +1,11 @@
-set(CTEST_PROJECT_NAME "vv_itk4")
-set(CTEST_NIGHTLY_START_TIME "1:00:00 UTC")
-
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=VV")
-set(CTEST_DROP_SITE_CDASH TRUE)
-
+# Set them in a dart file when using ctest to submit on an other dashboard
+# Those are set for running a classical make Experimental
+SET_IF_NOT_SET(CTEST_DROP_METHOD "http")
+SET_IF_NOT_SET(CTEST_DROP_SITE "my.cdash.org")
+SET_IF_NOT_SET(CTEST_DROP_LOCATION "/submit.php?project=VV")
+SET_IF_NOT_SET(CTEST_DROP_SITE_CDASH TRUE)
+SET_IF_NOT_SET(CTEST_PROJECT_NAME "vv_itk4")
+SET_IF_NOT_SET(CTEST_NIGHTLY_START_TIME "1:00:00 UTC")
 
 #=========================================================
 SET(CLITK_TEST_DATA_PATH ${PROJECT_BINARY_DIR}/tests/data/ CACHE INTERNAL DOCSTRING)
-# Get the data tests
-IF(WIN32)
-  SET(scriptExt bat)
-ELSE(UNIX)
-  SET(scriptExt sh)
-ENDIF()
-EXEC_PROGRAM(${PROJECT_SOURCE_DIR}/tests/fetch_data_test.${scriptExt} ARGS ${CLITK_TEST_DATA_PATH} http://russule/data/.git)
