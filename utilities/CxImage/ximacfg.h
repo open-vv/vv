550a5b880ff979562fc44389800b4be945e59fbb
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2012-03-21 18:38:29 +0100

    Change configuration for portability issues and only activate gif format

--- utilities/CxImage/ximacfg.h
+++ utilities/CxImage/ximacfg.h
@@ -2,42 +2,57 @@
 #define __ximaCFG_h
 
 /////////////////////////////////////////////////////////////////////////////
+// Protability issues
+#ifndef _tfopen
+#  define _tfopen fopen
+#endif
+#ifndef LPCTSTR
+#  define LPCTSTR char*
+#endif
+#ifndef HDC
+#  define HDC void*
+#endif
+#ifndef _tcsnicmp
+#  define _tcsnicmp strncasecmp
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
 // CxImage supported features
 #define CXIMAGE_SUPPORT_ALPHA          1
 #define CXIMAGE_SUPPORT_SELECTION      1
-#define CXIMAGE_SUPPORT_TRANSFORMATION 1
-#define CXIMAGE_SUPPORT_DSP            1
+#define CXIMAGE_SUPPORT_TRANSFORMATION 0
+#define CXIMAGE_SUPPORT_DSP            0
 #define CXIMAGE_SUPPORT_LAYERS		   1
-#define CXIMAGE_SUPPORT_INTERPOLATION  1
+#define CXIMAGE_SUPPORT_INTERPOLATION  0
 
 #define CXIMAGE_SUPPORT_DECODE	1
 #define CXIMAGE_SUPPORT_ENCODE	1		//<vho><T.Peck>
-#define	CXIMAGE_SUPPORT_WINDOWS 1
+#define	CXIMAGE_SUPPORT_WINDOWS 0
 
 /////////////////////////////////////////////////////////////////////////////
 // CxImage supported formats
-#define CXIMAGE_SUPPORT_BMP 1
+#define CXIMAGE_SUPPORT_BMP 0
 #define CXIMAGE_SUPPORT_GIF 1
-#define CXIMAGE_SUPPORT_JPG 1
-#define CXIMAGE_SUPPORT_PNG 1
-#define CXIMAGE_SUPPORT_ICO 1
-#define CXIMAGE_SUPPORT_TIF 1
-#define CXIMAGE_SUPPORT_TGA 1
-#define CXIMAGE_SUPPORT_PCX 1
-#define CXIMAGE_SUPPORT_WBMP 1
-#define CXIMAGE_SUPPORT_WMF 1
+#define CXIMAGE_SUPPORT_JPG 0
+#define CXIMAGE_SUPPORT_PNG 0
+#define CXIMAGE_SUPPORT_ICO 0
+#define CXIMAGE_SUPPORT_TIF 0
+#define CXIMAGE_SUPPORT_TGA 0
+#define CXIMAGE_SUPPORT_PCX 0
+#define CXIMAGE_SUPPORT_WBMP 0
+#define CXIMAGE_SUPPORT_WMF 0
 
-#define CXIMAGE_SUPPORT_JP2 1
-#define CXIMAGE_SUPPORT_JPC 1
-#define CXIMAGE_SUPPORT_PGX 1
-#define CXIMAGE_SUPPORT_PNM 1
-#define CXIMAGE_SUPPORT_RAS 1
+#define CXIMAGE_SUPPORT_JP2 0
+#define CXIMAGE_SUPPORT_JPC 0
+#define CXIMAGE_SUPPORT_PGX 0
+#define CXIMAGE_SUPPORT_PNM 0
+#define CXIMAGE_SUPPORT_RAS 0
 
 #define CXIMAGE_SUPPORT_JBG 0		// GPL'd see ../jbig/copying.txt & ../jbig/patents.htm
 
-#define CXIMAGE_SUPPORT_MNG 1
-#define CXIMAGE_SUPPORT_SKA 1
-#define CXIMAGE_SUPPORT_RAW 1
+#define CXIMAGE_SUPPORT_MNG 0
+#define CXIMAGE_SUPPORT_SKA 0
+#define CXIMAGE_SUPPORT_RAW 0
 
 /////////////////////////////////////////////////////////////////////////////
 #define	CXIMAGE_MAX_MEMORY 268435456
