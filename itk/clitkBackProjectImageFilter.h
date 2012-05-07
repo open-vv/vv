5f9b1f66850a409e87c1e131e8ae09a3541f05fe
Vivien Delmon <vivien.delmon@creatis.insa-lyon.fr>
2012-05-07 13:33:19 +0200

    itkv4 migration use itk::ThreadIdType

--- itk/clitkBackProjectImageFilter.h
+++ itk/clitkBackProjectImageFilter.h
@@ -242,7 +242,11 @@ namespace clitk
     void BeforeThreadedGenerateData(void );
   
     // Threaded Generate Data
+#if ITK_VERSION_MAJOR >= 4
+    void ThreadedGenerateData(const OutputImageRegionType & outputRegionForThread, itk::ThreadIdType threadId );
+#else
     void ThreadedGenerateData(const OutputImageRegionType & outputRegionForThread, int threadId );
+#endif
 
  
     //------------------------------------------------
