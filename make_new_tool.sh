93e40565c28c000d898fb5f013ffefc4ea51d863
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2013-06-07 17:08:08 +0200

    Fixed script for my mac

--- make_new_tool.sh
+++ make_new_tool.sh
@@ -10,7 +10,12 @@ find tools -name "clitkFooImage*" |
 while read i
 do
     cp $i ${i/FooImage/$1}
-    sed -i "s/FooImage/$1/ig" ${i/FooImage/$1}
+    if test "$(uname)" = "Darwin"
+    then
+        sed -i "" "s/FooImage/$1/ig" ${i/FooImage/$1}
+    else
+        sed -i "s/FooImage/$1/ig" ${i/FooImage/$1}
+    fi
 done
 echo "Done!"
 echo "Don't forget to add your new tool to the various CMakeLists.txt files."
