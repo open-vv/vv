8209f988394a0706a9f8834a325d687ec3555bd1
cervenansky <frederic.cervenansky@creatis.insa-lyon.fr>
2013-07-05 14:19:47 +0200

    Settings dialog to add new dicom server

---
+++ vv/vvPacsSettingsDialog.h
@@ -0,0 +1,22 @@
+#ifndef __vvPacsSettingsDialog_H
+#define __vvPacsSettingsDialog_H
+#include "ui_vvPacsSettingsDialog.h"
+#include <QFileDialog>
+#include <QTGUI/QWidget>
+
+ class vvPacsSettingsDialog : public QDialog 
+  {
+	  Q_OBJECT
+  public:
+	  
+	  vvPacsSettingsDialog(QWidget *parent=0);
+    
+	  ~vvPacsSettingsDialog(){}
+  private:
+	  QWidget *mparent;
+	  Ui::vvPacsSettingsDialog ui;
+private slots:
+	void accept();
+
+ };
+#endif //__vvPacsSettingsDialog_H
