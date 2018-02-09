03a3dde77c4de04ce6f0af2f8d6eda4cf0fb6e41
tbaudier <thomas.baudier@creatis.insa-lyon.fr>
2018-02-09 16:18:59 +0100

    Update Travis file to manage cache failure Add sonarcloud project

--- travis/travis_build_qt.sh
+++ travis/travis_build_qt.sh
@@ -14,10 +14,9 @@ elif [ "$QT_VERSION" == "5.5.1" ]; then
   if test $TRAVIS_OS_NAME == linux ; then sudo add-apt-repository --yes ppa:beineri/opt-qt551-trusty; fi
   if test $TRAVIS_OS_NAME == linux ; then sudo apt-get update -qq; fi
   if test $TRAVIS_OS_NAME == linux ; then sudo apt-get -y --force-yes install qt55tools; fi
-  
-  if test $TRAVIS_OS_NAME == osx ; then brew install qt@5.5; fi
-  if test $TRAVIS_OS_NAME == osx ; then brew link qt@5.5 --force; fi
-  if test $TRAVIS_OS_NAME == osx ; then brew linkapps qt@5.5; fi
 fi
+if test $TRAVIS_OS_NAME == osx ; then brew install qt5; fi
+if test $TRAVIS_OS_NAME == osx ; then brew link qt5 --force; fi
+if test $TRAVIS_OS_NAME == osx ; then brew linkapps qt5; fi
 
 
