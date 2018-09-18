632adadb8b9230458aeab8ddccc736d674edc87a
tbaudier <thomas.baudier@creatis.insa-lyon.fr>
2018-09-18 11:40:39 +0200

    Add VTK 8.1.1 and ITK 5 beta Clean travis script 4/n

--- travis/travis_build_qt.sh
+++ travis/travis_build_qt.sh
@@ -7,7 +7,7 @@ if [ "$QT_VERSION" == "4.8.7" ]; then
 elif [ "$QT_VERSION" == "5.5.1" ]; then
   if test $TRAVIS_OS_NAME == linux ; then sudo add-apt-repository --yes ppa:beineri/opt-qt551-trusty; fi
   if test $TRAVIS_OS_NAME == linux ; then sudo apt-get update -qq; fi
-  if test $TRAVIS_OS_NAME == linux ; then sudo apt-get -y --force-yes install qt55tools; fi
+  if test $TRAVIS_OS_NAME == linux ; then sudo apt-get -y --force-yes install qt55tools qt55x11extras; fi
 fi
 if test $TRAVIS_OS_NAME == osx ; then brew install qt5; fi
 if test $TRAVIS_OS_NAME == osx ; then brew link qt5 --force; fi
